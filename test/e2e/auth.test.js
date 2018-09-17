0e72af4ef008ee3b1a064657a91b3e3e112f0ad4
MariahAdams <mariah.adams23@yahoo.com>
2018-09-17 14:18:26 -0700

    successfully signs in a user

--- test/e2e/auth.test.js
+++ test/e2e/auth.test.js
@@ -26,4 +26,19 @@ describe.only('Auth API', () => {
     it('signup', () => {
         assert.ok(token);
     });
+
+    it('sigin', () => {
+        return request
+            .post('/api/auth/signing')
+            .send({
+                avatar: 'riveter',
+                username: 'mja23',
+                email: 'me@me.com',
+                password: 'abc123',
+                zip: 97306
+            })
+            .then(({ body }) => {
+                assert.ok(body.token);
+            });
+    });
 });
