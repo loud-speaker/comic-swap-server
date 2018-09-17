945f2609976a67d6c309dc1f71861fc7a6d4ddf1
MariahAdams <mariah.adams23@yahoo.com>
2018-09-17 11:45:54 -0700

    validates good model and required fields

--- test/unit/user.test.js
+++ test/unit/user.test.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 const User = require('../../lib/models/user');
-// const { getErrors } = require('./helpers');
+const { getErrors } = require('./helpers');
 
 describe.only('User model', () => {
 
@@ -21,6 +21,36 @@ describe.only('User model', () => {
     it('generates hash from password', () => {
         assert.ok(user.hash);
         assert.notEqual(user.hash, password);
+    });
+
+    it('compares password to hash', () => {
+        assert.isOk(user.comparePassword(password));
+    });
+
+    it('validates good model', () => {
+        const data = {
+            avatar: 'wonder woman',
+            username: 'mja23',
+            email: 'test@test.com',
+            password: 'abc123',
+            zip: 97302
+        };
+        const user = new User(data);
+
+        const json = user.toJSON();
+        delete json._id;
+        assert.deepEqual(json, data);
+        assert.isUndefined(user.validateSync());
+    });
+
+    it('validates required fields', () => {
+        const user = new User({});
+        const errors = getErrors(user.validateSync(), 4);
+
+        assert.equal(errors.username.kind, 'required');
+        assert.equal(errors.email.kind, 'required');
+        assert.equal(errors.password.kind, 'required');
+        assert.equal(errors.zip.kind, 'required');
     });
 
 });
