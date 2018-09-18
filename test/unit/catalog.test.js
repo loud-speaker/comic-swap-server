eca05fa09aa62b2cc67eb8dae758687b3e979ac7
MariahAdams <mariah.adams23@yahoo.com>
2018-09-18 14:16:10 -0700

    finished unit tests for catalog model

--- test/unit/catalog.test.js
+++ test/unit/catalog.test.js
@@ -3,7 +3,7 @@ const Catalog = require('../../lib/models/catalog');
 const { getErrors } = require('./helpers');
 const { Types } = require('mongoose');
 
-describe.only('Catalog model', () => {
+describe('Catalog model', () => {
 
     it('validates good model', () => {
         const data = {
@@ -25,6 +25,16 @@ describe.only('Catalog model', () => {
         const errors = getErrors(catalog.validateSync(), 1);
 
         assert.equal(errors.userId.kind, 'required');
+    });
+
+    it('returns comic condition', () => {
+        const catalog = new Catalog({
+            userId: Types.ObjectId(),
+            condition: 'banana'
+        });
+        const errors = getErrors(catalog.validateSync(), 1);
+
+        assert.equal(errors.condition.message, '`banana` is not a valid enum value for path `condition`.')
     });
 
 });
