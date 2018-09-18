d25b8c734012e75d1910e6ea98637944828b3603
Kevin Grayson <k.grayson.ms@gmail.com>
2018-09-17 22:26:50 -0700

    getOneComicDetail API call, processOneComicDetailData, and tests succesfful

---
+++ test/e2e/comicApi.test.js
@@ -0,0 +1,35 @@
+const { assert } = require('chai');
+// const request = require('./request');
+const { getOneComicDetail } = require('../../lib/services/comicsApi');
+
+
+describe('getOneComicDetail API call', () => {
+
+    let batmanEternal;
+
+    beforeEach(() => {
+        batmanEternal = {
+            id: 479928,
+            issueName: 'Batman... Eternal?',
+            volumeName: 'Batman Eternal',
+            coverDate: '2015-04-01'
+        };
+    });
+
+    it('Should get a comic by ID', () => {
+        return getOneComicDetail(batmanEternal.id)
+            .then(data => {
+                assert.isDefined(data);
+            });
+    });
+
+    it('Returns comic as processed data', () => {
+        return getOneComicDetail(batmanEternal.id)
+            .then(data => {
+                assert.deepEqual(data.issueName, batmanEternal.issueName);
+                assert.deepEqual(data.volumeName, batmanEternal.volumeName);
+                assert.deepEqual(data.coverDate, batmanEternal.coverDate);
+            });
+    });
+
+});
--- test/e2e/comicsApi.test.js
+++ test/e2e/comicsApi.test.js
@@ -1,17 +1,7 @@
 const { assert } = require('chai');
-// const request = require('./request');
-const { dropCollection  } = require('./_db');
-const { getComicsList } = require('./comicsTestApi');
+const { getComicsList } = require('../../lib/services/comicsApi');
 
-// const checkOk = res => {
-//     assert.equal(res.status, 200, 'expected 200 http status code');
-//     return res;
-// };
-
-
-describe.only('getComicsList API', () => {
-
-    beforeEach(() => dropCollection('comics'));
+describe('getComicsList API call', () => {
 
     it('Should get comic list', () => {
         return getComicsList()
